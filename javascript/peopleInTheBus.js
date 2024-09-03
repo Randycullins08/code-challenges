d09dfb647ab2aaa75ca24e0d6d3074bf7811561c
Randy Cullins <randycullins2018@gmail.com>
2024-09-03 14:09:30 -0600

    people in the bus challenge

---
+++ javascript/peopleInTheBus.js
@@ -0,0 +1,47 @@
+// There is a bus moving in the city which takes and drops some people at each bus stop.
+
+// You are provided with a list (or array) of integer pairs. Elements of each pair represent the number of people that get on the bus (the first item) and the number of people that get off the bus (the second item) at a bus stop.
+
+// Your task is to return the number of people who are still on the bus after the last bus stop (after the last array). Even though it is the last bus stop, the bus might not be empty and some people might still be inside the bus, they are probably sleeping there :D
+
+// Take a look on the test cases.
+
+const number = function (busStops) {
+  let peopleOnBus = 0;
+
+  for (let i = 0; i < busStops.length; i++) {
+    let [peopleGettingOn, peopleGettingOff] = busStops[i];
+    peopleOnBus += peopleGettingOn - peopleGettingOff;
+  }
+
+  return peopleOnBus;
+};
+
+console.log(
+  number([
+    [10, 0],
+    [3, 5],
+    [5, 8],
+  ])
+);
+console.log(
+  number([
+    [3, 0],
+    [9, 1],
+    [4, 10],
+    [12, 2],
+    [6, 1],
+    [7, 10],
+  ])
+);
+console.log(
+  number([
+    [3, 0],
+    [9, 1],
+    [4, 8],
+    [12, 2],
+    [6, 1],
+    [7, 8],
+  ])
+);
+console.log(number([[0, 0]]));
