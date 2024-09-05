fa01eac7263ac28f569364494fa4d7e504482efe
Randy Cullins <randycullins2018@gmail.com>
2024-09-05 13:24:38 -0600

    removed test cases

--- javascript/sumOfLowestNums.js
+++ javascript/sumOfLowestNums.js
@@ -13,5 +13,3 @@ function sumTwoSmallestNumbers(numbers) {
 console.log(sumTwoSmallestNumbers([5, 8, 12, 19, 22]));
 console.log(sumTwoSmallestNumbers([15, 28, 4, 2, 43]));
 console.log(sumTwoSmallestNumbers([3, 87, 45, 12, 7]));
-console.log(sumTwoSmallestNumbers([23, 71, 33, 82, 1]));
-console.log(sumTwoSmallestNumbers([52, 76, 14, 12, 4]));
