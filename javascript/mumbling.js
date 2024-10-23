ce2289237348941727e6bf482c5695597254724c
Randy Cullins <randycullins2018@gmail.com>
2024-10-23 15:12:11 -0600

    mumbling challenge

---
+++ javascript/mumbling.js
@@ -0,0 +1,19 @@
+// This time no story, no theory. The examples below show you how to write function accum:
+
+// Examples:
+// accum("abcd") -> "A-Bb-Ccc-Dddd"
+// accum("RqaEzty") -> "R-Qq-Aaa-Eeee-Zzzzz-Tttttt-Yyyyyyy"
+// accum("cwAt") -> "C-Ww-Aaa-Tttt"
+// The parameter of accum is a string which includes only letters from a..z and A..Z.
+
+function accum(s) {
+  return s
+    .split("") // Split the string into an array of characters
+    .map((char, index) => char.toUpperCase() + char.toLowerCase().repeat(index)) // Capitalize the first letter and repeat the lowercase version (index) times
+    .join("-"); // Join the result with hyphens
+}
+
+// Test cases
+console.log(accum("abcd")); // "A-Bb-Ccc-Dddd"
+console.log(accum("RqaEzty")); // "R-Qq-Aaa-Eeee-Zzzzz-Tttttt-Yyyyyyy"
+console.log(accum("cwAt")); // "C-Ww-Aaa-Tttt"
