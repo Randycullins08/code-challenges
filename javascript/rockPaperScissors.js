509d91cf0e9f541ec0e7a6fa7766ec6c35f2927c
Randy Cullins <randycullins2018@gmail.com>
2024-11-02 12:58:57 -0600

    cleaned up solution added

--- javascript/rockPaperScissors.js
+++ javascript/rockPaperScissors.js
@@ -7,19 +7,31 @@
 // "paper", "paper" --> "Draw!"
 
 function rockPaperScissors(player1, player2) {
+  const rules = {
+    rock: "paper",
+    paper: "scissors",
+    scissors: "rock",
+  };
+
   if (player1 === player2) {
     return "Draw!";
   }
 
-  if (
-    (player1 === "scissors" && player2 === "paper") ||
-    (player1 === "rock" && player2 === "scissors") ||
-    (player1 === "paper" && player2 === "rock")
-  ) {
+  if (player2 !== rules[player1]) {
     return "Player 1 won!";
   } else {
     return "Player 2 won!";
   }
+
+  //   if (
+  //     (player1 === "scissors" && player2 === "paper") ||
+  //     (player1 === "rock" && player2 === "scissors") ||
+  //     (player1 === "paper" && player2 === "rock")
+  //   ) {
+  //     return "Player 1 won!";
+  //   } else {
+  //     return "Player 2 won!";
+  //   }
 }
 
 // Examples
